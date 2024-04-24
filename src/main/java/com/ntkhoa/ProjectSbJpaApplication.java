1a16a4d26f54b016c383d68504ae3b732ef24f0e
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-04-24 16:39:37 +0700

    create all package

---
+++ src/main/java/com/ntkhoa/ProjectSbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ntkhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSbJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSbJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntkhoa/ProjectSbJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntkhoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSbJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
